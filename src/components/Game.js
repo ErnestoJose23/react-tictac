002bd682e707e8747e02df4100da3a12550dfbf5
Ernesto Custodio <ernestojosecg@gmail.com>
2020-07-02 12:11:41 +0200

    Game Component

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,5 +3,12 @@ import React from 'react';
 export default class Game extends React.Componenet {
     constructor(props) {
         super(props);
+        this.state = {
+            xIsNext: true,
+            stepNumber: 0,
+            history: [
+                { squares: Array(9).fill(null) }
+            ]
+        }
     }
 }
