cfae79bf4a020a2b90fa5b0df4456382d3a8af09
Ernesto Custodio <ernestojosecg@gmail.com>
2020-07-06 11:07:58 +0200

    Game

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,6 +15,9 @@ export default class Game extends React.Componenet {
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2 === 0),
-        })
+        });
+    }
+    handleClick(i) {
+        const history = trhis.state.history.slice(0, this.state.stepNumber + 1)
     }
 }
